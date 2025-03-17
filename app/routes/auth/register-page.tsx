90d4c3bd3824912506505554ffcca41106f5f0f0
Fernando Herrera <fernando.herrera85@gmail.com>
2025-03-17 12:39:49 -0400

    Agregar nueva ruta de prueba en routes.ts, actualizar el diseño del layout de chat para incluir enlaces de navegación y un botón de cierre de sesión, y mejorar las páginas de inicio de sesión y registro con enlaces de navegación adecuados.

--- app/routes/auth/register-page.tsx
+++ app/routes/auth/register-page.tsx
@@ -1,7 +1,9 @@
-import { Label } from '@radix-ui/react-label';
+import { Link } from 'react-router';
+
 import { Button } from '~/components/ui/button';
 import { Card, CardContent } from '~/components/ui/card';
 import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
 
 import placeholder from '~/assets/images/placeholder.svg';
 
@@ -91,10 +93,10 @@ const RegisterPage = () => {
                 </Button>
               </div>
               <div className="text-center text-sm">
-                Don&apos;t have an account?{' '}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
+                Already have an account?{' '}
+                <Link to="/auth/login" className="underline underline-offset-4">
+                  Log in
+                </Link>
               </div>
             </div>
           </form>
