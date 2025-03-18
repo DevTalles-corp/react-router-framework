1c501bd9cb1a5bed0f61a9000e0f9e2e41747636
Fernando Herrera <fernando.herrera85@gmail.com>
2025-03-18 09:14:30 -0400

    Renombrar componente de página de pruebas a 'MyRouteComponent' y actualizar los textos a 'Testing Page' y 'Testing Args' para mejorar la claridad y consistencia en la interfaz.

--- app/routes/auth/testing-args-page.tsx
+++ app/routes/auth/testing-args-page.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router';
-import type { Route } from './+types/testing-args-page';
+import type { Route } from './+types/testing-page';
 
-export default function TestingArgsPage({
+export default function MyRouteComponent({
   loaderData,
   actionData,
   params,
@@ -9,14 +9,14 @@ export default function TestingArgsPage({
 }: Route.ComponentProps) {
   return (
     <div>
-      <h1 className="font-bold text-2xl">Testing Args Page</h1>
+      <h1 className="font-bold text-2xl">Testing Page</h1>
       <p>Loader Data: {JSON.stringify(loaderData)}</p>
       <p>Action Data: {JSON.stringify(actionData)}</p>
       <p>Route Parameters: {JSON.stringify(params)}</p>
       <p>Matched Routes: {JSON.stringify(matches)}</p>
 
       <Link to="/auth/testing" className="text-blue-500 underline text-2xl">
-        Testing
+        Testing Args
       </Link>
     </div>
   );
