bec1c74f69aa2c74231fd7fd1f63bead9d47fe97
Fernando Herrera <fernando.herrera85@gmail.com>
2025-03-19 09:19:50 -0400

    Agregar lógica de redirección en la página de inicio de sesión y eliminar componentes de bienvenida obsoletos. Se implementa un loader para verificar la sesión del usuario y redirigir a la página de chat si ya está autenticado.

--- app/routes/auth/login-page.tsx
+++ app/routes/auth/login-page.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router';
+import { Link, redirect, useNavigate } from 'react-router';
 
 import { Button } from '~/components/ui/button';
 import { Card, CardContent } from '~/components/ui/card';
@@ -6,6 +6,20 @@ import { Label } from '~/components/ui/label';
 import { Input } from '~/components/ui/input';
 
 import placeholder from '~/assets/images/placeholder.svg';
+import type { Route } from './+types/login-page';
+
+import { getSession } from '~/sessions.server';
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await getSession(request.headers.get('Cookie'));
+  console.log('🚀 session', session.get('userId'));
+
+  if (session.get('userId')) {
+    return redirect('/chat');
+  }
+
+  // return null;
+}
 
 const LoginPage = () => {
   const navigate = useNavigate();
