a0ce9bd650776ca7ea9cd369593060156c41d773
Fernando Herrera <fernando.herrera85@gmail.com>
2025-03-20 11:32:21 -0400

    Actualizar la configuración de rutas para incluir prerenderizado de páginas de autenticación y modificar el componente de la página de pruebas para mostrar información detallada del usuario, incluyendo nombre, edad e ID. Se mejora la presentación de los datos en la interfaz.

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,13 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config';
+
+// Leer la base de datos de los clientes/productos
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+
+  async prerender() {
+    return ['/auth/login', '/auth/register', '/auth/testing'];
+  },
 } satisfies Config;
